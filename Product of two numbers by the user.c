33606a1c97ebb408f0df6f77594a7298288b1a3e
Koechfeli <101640843+Koechfeli@users.noreply.github.com>
2022-03-29 19:18:56 +0300

    Add files via upload

---
+++ Product of two numbers by the user.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    int product;
+
+
+    printf("Enter first number");
+    scanf("%d",&a);
+    printf("Enter second number");
+    scanf("%d",&b);
+
+    product=a*b;
+
+    printf("%d\n",product);
+    return 0;
+}
